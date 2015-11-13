786c5a0d9fb2d10f4904e68c955dd6f7970d3bad
Gabriel Montalvo <gmontalvoriv@users.noreply.github.com>
2015-11-13 15:25:17 -0400

    command eval change
    Some improvements were made. Doesn't get stuck when with incorrect commands now, displays help instead.

--- hns.js
+++ hns.js
@@ -5,34 +5,60 @@ var hn = require('node-hn');
 var cmd = require('commander');
 var pkg = require("./package.json");
 
+var cmdVal; // command value
+
 cmd
   .usage('<command> [-l LIMIT]')
   .version(pkg.version)
-  .option("-l, --limit [number]", "limit the number of posts to be displayed", /^[0-9]+$/)
-
+  .option('-l, --limit [number]', 'limit the number of posts to be displayed', /^[0-9]+$/)
 cmd
   .command('top')
   .description('Top stories')
-  .action(function () { hn.topstories(cmd.limit); });
-
 cmd
   .command('new')
   .description('New stories')
-  .action(function () { hn.newstories(cmd.limit); });
-
 cmd
   .command('show')
   .description('Show HN stories')
-  .action(function () { hn.showstories(cmd.limit); });
-
 cmd
   .command('ask')
   .description('Ask HN stories')
-  .action(function () { hn.askstories(cmd.limit); });
-
 cmd
   .command('jobs')
   .description('Job stories')
-  .action(function () { hn.jobstories(cmd.limit); });
+cmd
+  .arguments('<cmd>')
+  .action(function (cmd) { cmdVal = cmd; });
+
+cmd.on('--help', function() {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ hns top');
+  console.log('    $ hns top -l 3');
+  console.log('    $ hns show -l 5');
+  console.log('');
+});
 
 cmd.parse(process.argv);
+
+var l = cmd.limit;
+switch (cmdVal) {
+  case 'top':
+      hn.topstories(l); // node-hn library will evaluate the option's value
+      break;
+  case 'new':
+      hn.newstories(l);
+      break;
+  case 'show':
+      hn.showstories(l);
+      break;
+  case 'ask':
+      hn.askstories(l);
+      break;
+  case 'jobs':
+      hn.jobstories(l);
+      break;
+  default:
+      cmd.outputHelp();
+      process.exit(0);
+}
